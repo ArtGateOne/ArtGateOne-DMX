8d2bf6c250f53cc91a1a6ae80c67ee4f1ff428e9
ArtGateOne <65070703+ArtGateOne@users.noreply.github.com>
2023-07-25 06:50:19 +0200

    Update ArtGateOne_DMX_1.4.ino

--- ArtGateOne_DMX_1.4/ArtGateOne_DMX_1.4.ino
+++ ArtGateOne_DMX_1.4/ArtGateOne_DMX_1.4.ino
@@ -365,7 +365,7 @@ void loop() {
 
   // if there's data available, read a packet
   int packetSize = Udp.parsePacket();
-  if (packetSize == 14 || packetSize == 18 || packetSize == 24) {
+  if (packetSize == 14 || packetSize == 18) {
     // send a ArtPoolReply to the IP address and port that sent us the packet we received
     // Udp.beginPacket(Udp.remoteIP(), Udp.remotePort());
     Udp.beginPacket(0xFFFFFFFF, Udp.remotePort());
